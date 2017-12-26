f976cb5e6be8e7d5e7c6f3707a86ac977ab07bf1
T.MAHARANI <maharani1118@gmail.com>
2017-12-26 23:47:56 +0530

    Create schooladminheader.php

---
+++ SCHOOL ADMIN/schooladminheader.php
@@ -0,0 +1,34 @@
+<style type="text/css">
+.flash {
+   animation-name: flash;
+    animation-duration: 0.2s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    animation-play-state: running;
+}
+@keyframes flash {
+    from {color:#FF0066;}
+    to {color: black;}
+	
+</style>
+<body>
+ <div>
+   <table align="center" height="25%" width="100%" border="0" cellpadding="0" cellspacing="0" >
+     <tr>
+      <td align="center" height="25%" width="20%"><img src="images/schooladmin1.gif" height="80%" width="80%" /> </td>
+      <td align="center" height="50%" width="48%">
+	  <marquee behavior="alt" direction="up">
+	  <h2 class="flash"><font face="Arial Rounded MT" size="+3"> SCHOOL CONSTRUCTION TENDER </font></h2></marquee>
+      </td>
+      <td align="center" height="25%" width="15%"><img src="images/schooladmin4.gif" height="100%" width="100%" /> </td>
+     </td>
+      <td align="center" height="25%" width="30%"><img src="images/schooladmin2.gif" height="100%" width="100%" /> </td>
+     
+	 </tr>
+	 </table>
+  </div>
+   <hr color="#CC0066"> 
+    <h3 align="right" ><a href="admincomman.php">Back to Admin Home</a></h3>
+
+</body>
