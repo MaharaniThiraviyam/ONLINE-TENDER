a7c00741c15748112c4c6044ae453d7b4fb63799
T.MAHARANI <maharani1118@gmail.com>
2017-12-27 10:45:29 +0530

    Create railwaycompany.php

---
+++ RAILWAY COMPANY/railwaycompany.php
@@ -0,0 +1,38 @@
+<style type="text/css">
+hr.style7 {
+	border-top: 1px solid #8c8b8b;
+	border-bottom: 1px solid #fff;
+	border-color:blue;
+}
+	hr.style2 {
+	border-top: 3px double #8c8b8b;
+	border-color:blue;
+}
+.flash {
+   animation-name: flash;
+    animation-duration: 0.2s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    animation-play-state: running;
+}
+@keyframes flash {
+    from {color: blue;}
+    to {color: black;}
+
+
+</style>
+<body>
+ <div>
+   <table align="center" height="25%" width="90%" border="0" cellpadding="0" cellspacing="0" >
+     <tr>
+      <td align="center" height="25%" width="20%"><img src="images/railwaycompanyheader1.jpg" height="60%" width="70%" /> </td>
+      <td align="center" height="25%" width="30%"><img src="images/railwayadmin10.jpg" height="60%" width="100%" /> </td>
+	  <td align="center" height="25%" width="20%"><img src="images/railwayadmin2.gif" height="70%" width="90%" /> </td>
+	  
+      <td align="center" height="25%" width="20%"><img src="images/companycommon4.gif" height="70%" width="90%" /> </td>
+     </tr>
+	 </table>
+  </div>
+  <hr class="style2"  />
+</body>
