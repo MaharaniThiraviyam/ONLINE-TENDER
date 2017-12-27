58b54f78ee7d0f51b30019024fef40b3e69d14fe
T.MAHARANI <maharani1118@gmail.com>
2017-12-27 19:08:07 +0530

    Create slumadminheader.php

---
+++ SLUM ADMIN/slumadminheader.php
@@ -0,0 +1,34 @@
+<style type="text/css">
+	hr.style2 {
+	border-top: 3px double #8c8b8b;
+	border-color:#006600;
+}
+.flash {
+   animation-name: flash;
+    animation-duration: 0.2s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    animation-play-state: running;
+}
+@keyframes flash {
+    from {color:green;}
+    to {color: black;}
+
+
+</style>
+<body>
+ <div>
+   <table align="center" height="25%" width="90%" border="0" cellpadding="0" cellspacing="0" >
+     <tr>
+      <td align="center" height="20%" width="20%"><img src="images/img1.1.jpg" height="70%" width="90%" /> </td>
+      <td align="center" height="25%" width="40%"><marquee behavior="alt" direction="left">
+	  <h2 class="flash"><font face="Arial Rounded MT" size="+3"> SLUM CLEARANCE TENDER </font></h2></marquee></td>
+	 <td align="center" height="25%" width="20%"><img src="images/slumadmin3.gif" height="80%" width="100%" /> </td>
+     </tr>
+	 </table>
+  </div>
+  <hr class="style2"  />
+  <h3 align="right" ><a href="admincomman.php">Back to Admin Home</a></h3>
+
+</body>
