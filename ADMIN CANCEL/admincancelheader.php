6ab08ce558eb1fe0296054015ff973549933f015
T.MAHARANI <maharani1118@gmail.com>
2017-12-27 19:12:10 +0530

    Create admincancelheader.php

---
+++ ADMIN CANCEL/admincancelheader.php
@@ -0,0 +1,42 @@
+<style type="text/css">
+hr.style5 {
+	background-color:#FF0000;
+	border:groove;
+	border-color:#FF0066;
+	
+}
+
+	hr.style2 {
+	border-top: 3px double #8c8b8b;
+	border-color:#FF0066;
+}
+.flash {
+   animation-name: flash;
+    animation-duration: 0.2s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    animation-play-state: running;
+}
+@keyframes flash {
+    from {color:#FF0066;}
+    to {color: black;}
+
+
+</style>
+<body>
+ <div>
+   <table align="center" height="25%" width="90%" border="0" cellpadding="0" cellspacing="0" >
+     <tr>
+      <td align="center" height="25%" width="15%"><img src="images/img3.2.jpg" height="50%" width="100%" /> </td>
+	   <td align="center" height="25%" width="20%"><img src="images/cancel.jpg" height="50%" width="70%" /> </td>
+     
+      <td align="center" height="25%" width="20%"><img src="images/schoolcompany.gif" height="70%" width="70%" /> </td>
+       <td align="center" height="25%" width="20%"><img src="images/companysearch4.gif" height="70%" width="70%" /> </td>
+     
+	  </tr>
+	 </table>
+  </div>
+  <hr class="style5"  />
+  
+</body>
