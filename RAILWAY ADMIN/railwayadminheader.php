0ab5b0249c15ac7877e6863578ffdea021fc5f18
T.MAHARANI <maharani1118@gmail.com>
2017-12-27 10:38:36 +0530

    Create railwayadminheader.php

---
+++ RAILWAY ADMIN/railwayadminheader.php
@@ -0,0 +1,40 @@
+<style type="text/css">
+hr.style7 {
+	border-top: 1px solid #8c8b8b;
+	border-bottom: 1px solid #fff;
+	border-color:blue;
+}
+	hr.style2 {
+	border-top: 3px double #8c8b8b;
+	border-color:blue;
+}
+.flash {
+   animation-name: flash;
+    animation-duration: 0.2s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    animation-play-state: running;
+}
+@keyframes flash {
+    from {color: blue;}
+    to {color: black;}
+
+
+</style>
+<body>
+ <div>
+   <table align="center" height="25%" width="90%" border="0" cellpadding="0" cellspacing="0" >
+     <tr>
+      <td align="center" height="25%" width="20%"><img src="images/img1.2.jpg" height="80%" width="100%" /> </td>
+      <td align="center" height="25%" width="30%"><img src="images/railwayadmin10.jpg" height="80%" width="100%" /> </td>
+	  <td align="center" height="25%" width="20%"><img src="images/railwayadmin1.gif" height="90%" width="100%" /> </td>
+	  
+      <td align="center" height="25%" width="20%"><img src="images/railwayadmin6.gif" height="90%" width="90%" /> </td>
+     </tr>
+	 </table>
+  </div>
+  <hr class="style2"  />
+    <h3 align="right" ><a href="admincomman.php">Back to Admin Home</a></h3>
+
+</body>
